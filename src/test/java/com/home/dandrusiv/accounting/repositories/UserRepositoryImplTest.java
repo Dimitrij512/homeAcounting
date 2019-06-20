0d2631bd38682b0295796c806bac84c4a66e4ef8
dandrusiv <dandrusiv@homeaway.com>
2019-06-20 17:43:53 +0300

    formatin and adding test to user repository

--- src/test/java/com/home/dandrusiv/accounting/repositories/UserRepositoryImplTest.java
+++ src/test/java/com/home/dandrusiv/accounting/repositories/UserRepositoryImplTest.java
@@ -2,7 +2,7 @@ package com.home.dandrusiv.accounting.repositories;
 
 import java.util.List;
 
-import com.home.dandrusiv.accounting.models.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.home.dandrusiv.accounting.models.User;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,9 +22,16 @@ public class UserRepositoryImplTest {
     @Autowired
     UserRepositoryImpl repository;
 
+    private User preparedUser;
+
+    @Before
+    public void setup() {
+        repository.deleteAll();
+        preparedUser = prepareUser("test@email.com", "TestName", "TestLastName");
+    }
+
     @Test
     public void testCreateUser() {
-        User preparedUser = prepareUser("test@email.com");
         User createdUser = repository.create(preparedUser);
 
         assertThat(preparedUser).isEqualToIgnoringGivenFields(createdUser, "id");
@@ -31,7 +39,7 @@ public class UserRepositoryImplTest {
 
     @Test
     public void testUpdateUser() {
-        User createdUser = repository.create(prepareUser("test@email.com"));
+        User createdUser = repository.create(preparedUser);
         createdUser.setEmail("other@email.com");
 
         User updatedUser = repository.update(createdUser);
@@ -41,8 +49,51 @@ public class UserRepositoryImplTest {
         assertThat(createdUser).isEqualTo(updatedUser);
     }
 
+    @Test
+    public void testGetById() {
+        User createdUser = repository.create(preparedUser);
+        User userById = repository.getById(createdUser.getId());
 
-    private User prepareUser(String email){
+        assertThat(createdUser).isEqualTo(userById);
+    }
+
+    @Test
+    public void testGetByEmail() {
+        User createdUser = repository.create(preparedUser);
+        User userByEmail = repository.getByEmail(createdUser.getEmail());
+
+        assertThat(createdUser).isEqualTo(userByEmail);
+    }
+
+    @Test
+    public void testFindByFirstName() {
+        User createdUser = repository.create(preparedUser);
+        List<User> userList = repository.findByFirstName(createdUser.getFirstName());
+
+        assertThat(userList).hasSize(1);
+        assertThat(userList).contains(createdUser);
+    }
+
+    @Test
+    public void testFindByLastName() {
+        User createdUser = repository.create(preparedUser);
+        List<User> userList = repository.findByLastName(createdUser.getLastName());
+
+        assertThat(userList).hasSize(1);
+        assertThat(userList).contains(createdUser);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        User createdUser = repository.create(preparedUser);
+        repository.delete(createdUser.getId());
+
+        List<User> userList = repository.findAll();
+
+        assertThat(userList).isEmpty();
+    }
+
+    private User prepareUser(String email, String name, String lastName) {
         User user = new User();
         user.setEmail(email);
         user.setFirstName("Test");
