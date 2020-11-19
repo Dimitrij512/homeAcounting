9e1f2807465b586d921ab9092c4d59c787ce047f
dandrusiv <dmytro.andrusiv@gmail.com>
2020-11-19 16:54:10 +0200

    add cross origin

--- src/main/java/com/home/dandrusiv/accounting/controllers/ItemController.java
+++ src/main/java/com/home/dandrusiv/accounting/controllers/ItemController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.home.dandrusiv.accounting.models.Item;
 import com.home.dandrusiv.accounting.services.ItemService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("item")
 public class ItemController {
 
@@ -43,7 +45,11 @@ public class ItemController {
     @GetMapping("/id/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Item getById(@PathVariable String id) {
-        return service.getById(id);
+
+        System.out.println("get item by id");
+        Item byId = service.getById(id);
+        System.out.println("Item" + byId.toString());
+        return byId;
     }
 
     @GetMapping("/categoryId/{id}")
--- src/main/java/com/home/dandrusiv/accounting/controllers/UserController.java
+++ src/main/java/com/home/dandrusiv/accounting/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.home.dandrusiv.accounting.controllers;
 import com.home.dandrusiv.accounting.models.User;
 import com.home.dandrusiv.accounting.repositories.UserRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("user")
 public class UserController {
 
