6ada02725282a3d0760658be76d4e3dce06d225e
dandrusiv <dmytro.andrusiv@gmail.com>
2020-12-16 14:36:11 +0200

    fix serialization of ItemDto

--- src/main/java/com/home/dandrusiv/accounting/dto/ItemDto.java
+++ src/main/java/com/home/dandrusiv/accounting/dto/ItemDto.java
@@ -3,14 +3,16 @@ package com.home.dandrusiv.accounting.dto;
 import java.util.Date;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class ItemDto {
-    private String id;
-    private String name;
-    private double value;
-    private String comment;
-    Date date;
-    String categoryId;
-    String categoryName;
+    private final String id;
+    private final String name;
+    private final double value;
+    private final String comment;
+    private final Date date;
+    private final String categoryId;
+    private final String categoryName;
 }
